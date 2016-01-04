829c6b4cbf0fbe3ee084b6a1f40872c85561fc2f
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-04 21:02:05 +0300

    Добавлен фиктивный JSON задачи

---
+++ TaskBerserkTests/Dummy.swift
@@ -0,0 +1,22 @@
+//
+//  Dummy.swift
+//  TaskBerserk
+//
+//  Created by Александр on 04.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+let taskJSON: [String: AnyObject] = [
+    "description": "Task description",
+    "id": 123,
+    "project": "Shopping list",
+    "urgency": 2.31,
+    "status": "waiting",
+    "uuid": "b8d05cfe-8464-44ef-9d99-eb3e7809d337",
+    "due": "Apr 1, 2015, 8:53 AM",
+    "tags": ["first tag", "second tag"],
+    "depends": 251,
+    "priority": "H"
+]
