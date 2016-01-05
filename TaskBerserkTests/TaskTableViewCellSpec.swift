d3b789fc755f162498db328562cd0f875ed365cc
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-05 23:38:08 +0300

    Добавлены тесты проверяющие корректность логики представления

---
+++ TaskBerserkTests/TaskTableViewCellSpec.swift
@@ -0,0 +1,31 @@
+//
+//  TaskTableViewCellSpec.swift
+//  TaskBerserk
+//
+//  Created by Александр on 05.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Himotoki
+@testable import TaskBerserk
+
+class TaskTableViewCellSpec: QuickSpec {
+    override func spec() {
+        var viewModel: TaskTableViewCellModel!
+        
+        beforeEach {
+            viewModel = TaskTableViewCellModel(task: dummyResponse.tasks[0])
+        }
+        
+        it("correctly formats tags") {
+            expect(viewModel.tagsText).toEventually(equal("@computer, online"))
+        }
+        
+        it("sets status and description") {
+            expect(viewModel.description).toEventually(equal("Task 1 description"))
+            expect(viewModel.status).toEventually(equal("waiting"))
+        }
+    }
+}
