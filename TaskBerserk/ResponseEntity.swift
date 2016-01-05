19c3bce0e68954a2ee0e950c426625689e900780
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-05 12:11:58 +0300

    Добавлена сущность ответа на запрос

---
+++ TaskBerserk/ResponseEntity.swift
@@ -0,0 +1,49 @@
+//
+//  ResponseEntity.swift
+//  TaskBerserk
+//
+//  Created by Александр on 05.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Himotoki
+
+class ResponseEntity {
+    let totalCount: Int64
+    let tasks: [TaskEntity]
+    
+    init(totalCount: Int64, tasks: [TaskEntity]) {
+        self.totalCount = totalCount
+        self.tasks = tasks
+    }
+}
+
+// MARK: Decodable
+extension ResponseEntity: Decodable {
+    static func decode(e: Extractor) throws -> ResponseEntity {
+        return try ResponseEntity(
+            totalCount: e <| "total_count",
+            tasks: e <|| "objects")
+    }
+}
+
+/*
+
+{
+    "meta":{
+    "limit":100,
+    "next":null,
+    "offset":0,
+    "previous":null,
+    "total_count":5
+},
+    "objects":[
+        {},
+        {},
+        {},
+        {},
+        {}
+    ]
+}
+
+*/
