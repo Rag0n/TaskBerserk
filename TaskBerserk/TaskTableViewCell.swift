22380b0fdebf7783096ce90b38f8cf98c62afe8d
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-05 20:08:34 +0300

    Добавлены пустые реализации

---
+++ TaskBerserk/TaskTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskTableViewCell.swift
+//  TaskBerserk
+//
+//  Created by Александр on 05.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+    
+}
---
+++ TaskBerserk/TasksTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TasksTableViewController.swift
+//  TaskBerserk
+//
+//  Created by Александр on 05.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+
+class TasksTableViewController: UITableViewController {
+    
+}
