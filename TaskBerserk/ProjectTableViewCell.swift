a822800dfe51e163f95ca6a483cf53398ace499e
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-05 20:09:08 +0300

    Добавлены пустые реализации

---
+++ TaskBerserk/ProjectTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectTableViewCell.swift
+//  TaskBerserk
+//
+//  Created by Александр on 05.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+
+private final class ProjectTableViewCell: UITableViewCell {
+    
+}
