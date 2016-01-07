bd36ccd393ca70d3160d03f11bdff5fec6ed4ef4
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-07 23:38:05 +0300

    Добавлен биндинг к viewModel.cells, реализавон TableViewDataSource

--- TaskBerserk/ProjectsTableViewController.swift
+++ TaskBerserk/ProjectsTableViewController.swift
@@ -7,7 +7,41 @@
 //
 
 import UIKit
+import RxSwift
 
 final class ProjectsTableViewController: UITableViewController {
+    private let disposeBag = DisposeBag()
+    private var cells: [ProjectTableViewCellModeling]?
     
+    private struct Constants {
+        static let projectCellIdentifier = "ProjectTableViewCell"
+    }
+    
+    var viewModel: ProjectsTableViewModeling? {
+        didSet {
+            viewModel?.cellModels.bindNext { cellModels in
+                self.cells = cellModels
+                self.tableView.reloadData()
+            }.addDisposableTo(disposeBag)
+        }
+    }
+}
+
+// MARK: UITableViewDataSource
+extension ProjectsTableViewController {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Constants.projectCellIdentifier, forIndexPath: indexPath) as! ProjectTableViewCell
+        
+        if let cells = cells {
+            cell.viewModel = cells[indexPath.row]
+        } else {
+            cell.viewModel = nil
+        }
+        
+        return cell
+    }
 }
