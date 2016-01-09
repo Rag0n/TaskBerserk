45dc1b09b1e01623439c98beb09d26134c645b7d
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-09 13:59:11 +0300

    Добавлена реализация создания managedObjextContext

---
+++ TaskBerserk/CoreDataStack.swift
@@ -0,0 +1,29 @@
+//
+//  CoreDataStack.swift
+//  TaskBerserk
+//
+//  Created by Александр on 09.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+private let storeURL = NSURL.documentsURL.URLByAppendingPathComponent("TaskBerserk.taskberserk")
+
+func createMainContext() -> NSManagedObjectContext {
+    // get bundle where object model resides
+    let bundles = [NSBundle(forClass: Task.self)]
+    guard let model = NSManagedObjectModel.mergedModelFromBundles(bundles) else {
+        fatalError("Model is not found")
+    }
+    
+    // configure managed context
+    let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
+    try! psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: nil)
+    
+    let context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+    context.persistentStoreCoordinator = psc
+    
+    return context
+}
