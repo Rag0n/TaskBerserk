a34d38932f971788216b846faa5038060feb8050
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-09 13:43:45 +0300

    Добавлено расширение documentsURL

---
+++ TaskBerserk/NSURL+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  NSURL+extensions.swift
+//  TaskBerserk
+//
+//  Created by Александр on 09.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+extension NSURL {
+    static var documentsURL: NSURL {
+        return try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+    }
+}
