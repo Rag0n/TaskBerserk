bf6b27897f32a4b1b82a0733abfac875a374d467
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-09 16:43:14 +0300

    Добавлена пустая реализация класса, инкапсулирующая методы FethedResultsControllerDelegate

---
+++ TaskBerserk/FetchedResultsDataProvider.swift
@@ -0,0 +1,15 @@
+//
+//  FetchedResultsDataProvider.swift
+//  TaskBerserk
+//
+//  Created by Александр on 09.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// encapsulates FetchedResultsController delegates methods
+class FetchedResultsDataProvider<Delegate: DataProviderDelegate>: NSObject, NSFetchedResultsControllerDelegate, DataProvider {
+    
+}
