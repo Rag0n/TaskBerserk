7c6c6850d321b131766dd64953072ae40db868bf
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-09 13:19:39 +0300

    Добавлена пустая реализация

---
+++ TaskBerserk/ManagedObject.swift
@@ -0,0 +1,14 @@
+//
+//  ManagedObject.swift
+//  TaskBerserk
+//
+//  Created by Александр on 09.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ManagedObject: NSManagedObject {
+    
+}
