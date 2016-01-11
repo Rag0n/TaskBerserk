91238efe20e0e9075e22ae20ec90ffc1439dcdf3
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-11 22:15:46 +0300

    Реализована функция findOrCreateProject

--- TaskBerserk/Project.swift
+++ TaskBerserk/Project.swift
@@ -13,6 +13,15 @@ final class Project: ManagedObject {
     @NSManaged private(set) var name: String
     @NSManaged private(set) var updatedAt: NSDate
     @NSManaged private(set) var tasks: Set<Task>
+    
+    static func findOrCreateProject(projectName: String, inContext moc: NSManagedObjectContext) -> Project {
+        let predicate = NSPredicate(format: "name = %@", projectName)
+        let project = findOrCreateInContext(moc, matchingPredicate: predicate) { project in
+            project.name = projectName
+            project.updatedAt = NSDate()
+        }
+        return project
+    }
 }
 
 // MARK: ManagedObjectType
