d06b42ede9777c927b9d8eb255e6ffc071ae737f
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-11 16:37:58 +0300

    Реализована базовая vm для taskDetail

--- TaskBerserk/TaskDetailViewModel.swift
+++ TaskBerserk/TaskDetailViewModel.swift
@@ -7,19 +7,46 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class TaskDetailViewModel: TaskDetailViewModeling {
-    var desc: String
-    var status: String
-    var tagsText: String
-    var urgency: String
-    var priority: String
+    
+    // TaskDetailViewModel
+    
+    var desc: Observable<String> {
+        return _desc.asObservable()
+    }
+    
+    var status: Observable<String> {
+        return _status.asObservable()
+    }
+    
+    var tagsText: Observable<String> {
+        return _tagsText.asObservable()
+    }
+    
+    var urgency: Observable<String> {
+        return _urgency.asObservable()
+    }
+    
+    var priority: Observable<String> {
+        return _priority
+    }
     
     init(task: Task) {
-        self.desc = task.desc
-        self.status = task.status
-        self.tagsText = task.tags?.joinWithSeparator(", ") ?? ""
-        self.urgency = "\(task.urgency)"
-        self.priority = task.priority ?? "No priority"
+        _desc.onNext(task.desc)
+        _status.onNext(task.status)
+        _tagsText.onNext(task.tags?.joinWithSeparator(", ") ?? "")
+        _urgency.onNext("\(task.urgency)")
+        _priority.onNext(task.priority ?? "No priority")
     }
+    
+    // MARK: Private
+    
+    private let _desc = BehaviorSubject<String>(value: "")
+    private let _status = BehaviorSubject<String>(value: "")
+    private let _tagsText = BehaviorSubject<String>(value: "")
+    private let _urgency = BehaviorSubject<String>(value: "")
+    private let _priority = BehaviorSubject<String>(value: "")
 }
--- TaskBerserk/TaskDetailViewModeling.swift
+++ TaskBerserk/TaskDetailViewModeling.swift
@@ -7,11 +7,12 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol TaskDetailViewModeling {
-    var desc: String { get }
-    var status: String { get }
-    var tagsText: String { get }
-    var urgency: String { get }
-    var priority: String { get }
+    var desc: Observable<String> { get }
+    var status: Observable<String> { get }
+    var tagsText: Observable<String> { get }
+    var urgency: Observable<String> { get }
+    var priority: Observable<String> { get }
 }
