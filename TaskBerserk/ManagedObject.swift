2da04c39036dd33169f1b944a08ac14d44272e41
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-11 21:32:19 +0300

    Добавлено расширение ManagedObjectType, реализуюшее методы поиска объекта в контексте/бд, либо создания нового объекта

--- TaskBerserk/ManagedObject.swift
+++ TaskBerserk/ManagedObject.swift
@@ -29,3 +29,62 @@ extension ManagedObjectType {
         return request
     }
 }
+
+extension ManagedObjectType where Self: ManagedObject {
+    
+    static func findOrCreateInContext(moc: NSManagedObjectContext, matchingPredicate predicate: NSPredicate, configure: Self -> ()) -> Self {
+        // returns existing object(object can be in context or in db)
+        guard let obj = findOrFetchInContext(moc, matchingPredicate: predicate) else {
+            // if doesnt exitst, then create and configure
+            let newObject: Self = moc.insertObject()
+            configure(newObject)
+            return newObject
+        }
+        return obj
+    }
+    
+    /// Checks if required object is already registered in the context(performance optimization)
+    static func findOrFetchInContext(moc: NSManagedObjectContext, matchingPredicate predicate: NSPredicate) -> Self? {
+        // return if object already in context
+        guard let obj = materializedObjectInContext(moc, matchingPredicate: predicate) else {
+            // else execute a fetch request
+            return fetchInContext(moc) { request in
+                request.predicate = predicate
+                request.returnsObjectsAsFaults = false
+                request.fetchLimit = 1
+                }.first
+        }
+        return obj
+    }
+    
+    /// executes a fetch request
+    static func fetchInContext(context: NSManagedObjectContext, @noescape configurationBlock: NSFetchRequest -> () = { _ in }) -> [Self] {
+        let request = NSFetchRequest(entityName: Self.entityName)
+        configurationBlock(request)
+        guard let result = try! context.executeFetchRequest(request) as? [Self] else {
+            fatalError("Fetched objects have wrong type")
+        }
+        return result
+    }
+    
+    static func countInContext(context: NSManagedObjectContext, @noescape configurationBlock: NSFetchRequest -> () = { _ in }) -> Int {
+        let request = NSFetchRequest(entityName: entityName)
+        configurationBlock(request)
+        var error: NSError?
+        let result = context.countForFetchRequest(request, error: &error)
+        guard result != NSNotFound else {
+            fatalError("Failed to execute fetch request: \(error)")
+        }
+        return result
+    }
+    
+    /// searches object in context(memory)
+    static func materializedObjectInContext(moc: NSManagedObjectContext, matchingPredicate predicate: NSPredicate) -> Self? {
+        for obj in moc.registeredObjects where !obj.fault {
+            guard let res = obj as? Self where predicate.evaluateWithObject(res) else { continue }
+            return res
+        }
+        return nil
+    }
+    
+}
