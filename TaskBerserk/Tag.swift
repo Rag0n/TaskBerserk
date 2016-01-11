827f81641a13adafdc26caadcf8e9e12c88311c0
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-11 22:16:16 +0300

    Реализована функция findOrCreateTags

--- TaskBerserk/Tag.swift
+++ TaskBerserk/Tag.swift
@@ -13,6 +13,25 @@ final class Tag: ManagedObject {
     @NSManaged private(set) var name: String
     @NSManaged private(set) var updatedAt: NSDate
     @NSManaged private(set) var tasks: Set<Task>
+    
+    static func findOrCreateTags(tagNames: [String], inContext moc: NSManagedObjectContext) -> [Tag] {
+        var tags = [Tag]()
+        
+        for tagName in tagNames {
+            tags.append(Tag.findOrCreateTag(tagName, inContext: moc))
+        }
+        
+        return tags
+    }
+    
+    static func findOrCreateTag(tagName: String, inContext moc: NSManagedObjectContext) -> Tag {
+        let predicate = NSPredicate(format: "name == %@", tagName)
+        let tag = findOrCreateInContext(moc, matchingPredicate: predicate) { tag in
+            tag.name = tagName
+            tag.updatedAt = NSDate()
+        }
+        return tag
+    }
 }
 
 // MARK: ManagedObjectType
