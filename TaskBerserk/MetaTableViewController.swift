7d1f22b817f1714c245583442fce36e858dbe4de
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-12 21:02:10 +0300

    Добавлены outlet, расширение UISegmentedControl

--- TaskBerserk/MetaTableViewController.swift
+++ TaskBerserk/MetaTableViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class MetaTableViewController: UITableViewController {
     @IBOutlet weak var filterSegmentedControl: UISegmentedControl!
     
+    override func viewDidLoad() {
+        filterSegmentedControl.rx_controlEvent(.ValueChanged)
+            .subscribeNext {
+                
+            }
+    }
+    
+    private let disposeBag = DisposeBag()
+}
+
+private enum MetaFilter: Int {
+    case Projects = 0
+    case Tags
+    case Contexts
+}
+
+extension UISegmentedControl {
+    private var metaFilter: MetaFilter {
+        guard let mf = MetaFilter(rawValue: selectedSegmentIndex) else {
+            fatalError("Invalid filter index")
+        }
+        return mf
+    }
 }
