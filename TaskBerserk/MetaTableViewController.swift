203852e63a988edb5f4aceb9185cc8f73e0a32f8
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-12 16:16:58 +0300

    Добавлен TabBarVC, добавлен пустой metaTableView, удалены ненужные VC из storyboard(splitView)

---
+++ TaskBerserk/MetaTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MetaTableViewController.swift
+//  TaskBerserk
+//
+//  Created by Александр on 12.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class MetaTableViewController: UITableViewController {
+    
+}
