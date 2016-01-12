3023ca4d14fab211b54979b8d51020a134464588
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-12 21:04:23 +0300

    Настроен fetchedResultsControllerDelegate

--- TaskBerserk/MetaTableViewModel.swift
+++ TaskBerserk/MetaTableViewModel.swift
@@ -10,4 +10,39 @@ import Foundation
 import RxSwift
 import CoreData
 
+class MetaTableViewModel: MetaTableViewModeling, DataProviderDelegate {
+    typealias Object = NameWithCountRepresentable
+    typealias ViewModel = MetaTableViewCellModeling
+    
+    var managedObjectContext: NSManagedObjectContext!
+    var updates: Observable<[DataProviderUpdate<MetaTableViewCellModeling>]> {
+        return dataProvider.updatesSignal
+    }
+    
+    init(managedObject: NSManagedObjectContext) {
+        self.managedObjectContext = managedObject
+        setupDataProvider()
+    }
+    
+    func numberOfItemsInSection(section: Int) -> Int {
+        return dataProvider.numberOfItemsInSection(section)
+    }
+    
+    private func setupDataProvider() {
+        let request = Project.sortedFetchRequest
+        request.returnsObjectsAsFaults = false
+        request.fetchBatchSize = 20
+        let frc = NSFetchedResultsController(fetchRequest: request,
+            managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        let transformerFunc: (Object) -> ViewModel = { object in
+            MetaTableViewCellModel(object: object)
+        }
+        
+        dataProvider = FetchedResultsDataProvider(fetchedResultsController: frc, delegate: self, transformerFunc: transformerFunc)
+    }
 
+    // MARK: Private
+    private var dataProvider: FetchedResultsDataProvider<MetaTableViewModel>!
+    private let disposeBag = DisposeBag()
+}
