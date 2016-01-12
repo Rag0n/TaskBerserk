85f58f7bf9cd003ed7ef019e71d58036dbdf9367
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-12 23:10:36 +0300

    Представление изменено на статическую таблицу, добавлены outlets

--- TaskBerserk/TaskDetailViewController.swift
+++ TaskBerserk/TaskDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class TaskDetailViewController: UIViewController {
+class TaskDetailViewController: UITableViewController {
     
     var viewModel: TaskDetailViewModeling!
     
@@ -25,48 +25,56 @@ class TaskDetailViewController: UIViewController {
     
     // MARK: IBOutlets
     
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var projectTextField: UITextField!
+    
+    @IBOutlet weak var priorityNoneButton: UIButton!
+    @IBOutlet weak var priorityLowButton: UIButton!
+    @IBOutlet weak var priorityMediumButton: UIButton!
+    @IBOutlet weak var priorityHighButton: UIButton!
+    
     @IBOutlet weak var urgencyLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var tagsLabel: UILabel!
-    @IBOutlet weak var priorityButton: UIButton!
-    @IBOutlet weak var statusButton: UIButton!
-    @IBOutlet weak var addTagButton: UIButton!
+    
     @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     
     // MARK: Private
     
     private let disposeBag = DisposeBag()
     
     private func bindToViewModel() {
-        viewModel.name
-            .bindTo(descriptionLabel.rx_text)
-            .addDisposableTo(disposeBag)
-        
-        viewModel.tagsText
-            .bindTo(tagsLabel.rx_text)
-            .addDisposableTo(disposeBag)
-        
-        viewModel.urgency
-            .bindTo(urgencyLabel.rx_text)
-            .addDisposableTo(disposeBag)
-    
-        viewModel.status
-            .subscribeNext { title in
-                self.statusButton.setTitle(title, forState: .Normal)
-            }
-            .addDisposableTo(disposeBag)
-        
-        viewModel.status
-            .subscribeNext { title in
-                self.priorityButton.setTitle(title, forState: .Normal)
-            }
-            .addDisposableTo(disposeBag)
-        
-        viewModel.popViewController
-            .filter { $0 == true }
-            .subscribeNext {_ in
-                self.navigationController?.popViewControllerAnimated(true)
-            }
-            .addDisposableTo(disposeBag)
+//        viewModel.name
+//            .bindTo(descriptionLabel.rx_text)
+//            .addDisposableTo(disposeBag)
+//        
+//        viewModel.tagsText
+//            .bindTo(tagsLabel.rx_text)
+//            .addDisposableTo(disposeBag)
+//        
+//        viewModel.urgency
+//            .bindTo(urgencyLabel.rx_text)
+//            .addDisposableTo(disposeBag)
+//    
+//        viewModel.status
+//            .subscribeNext { title in
+//                self.statusButton.setTitle(title, forState: .Normal)
+//            }
+//            .addDisposableTo(disposeBag)
+//        
+//        viewModel.status
+//            .subscribeNext { title in
+//                self.priorityButton.setTitle(title, forState: .Normal)
+//            }
+//            .addDisposableTo(disposeBag)
+//        
+//        viewModel.popViewController
+//            .filter { $0 == true }
+//            .subscribeNext {_ in
+//                self.navigationController?.popViewControllerAnimated(true)
+//            }
+//            .addDisposableTo(disposeBag)
     }
 }
