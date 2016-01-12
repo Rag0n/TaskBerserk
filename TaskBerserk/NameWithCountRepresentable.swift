6d0906cd1343dbdd704fea5e2f495045148f5159
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-12 21:00:17 +0300

    Добавлен протокол с двумя свойствами name/count

---
+++ TaskBerserk/NameWithCountRepresentable.swift
@@ -0,0 +1,14 @@
+//
+//  NameWithCountRepresentable.swift
+//  TaskBerserk
+//
+//  Created by Александр on 12.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+protocol NameWithCountRepresentable {
+    var nameString: String { get }
+    var countString: String { get }
+}
