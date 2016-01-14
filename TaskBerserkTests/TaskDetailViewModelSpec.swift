80423d9bddf9462f86f2e7c2d601ee89c4bd469a
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-14 23:48:46 +0300

    Добавлен пустой класс тестов

---
+++ TaskBerserkTests/TaskDetailViewModelSpec.swift
@@ -0,0 +1,26 @@
+//
+//  TaskDetailViewModelSpec.swift
+//  TaskBerserk
+//
+//  Created by Александр on 14.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Quick
+import Nimble
+import CoreData
+@testable import TaskBerserk
+
+class TaskDetailViewModelSpec: QuickSpec {
+    override func spec() {
+        var managedObjectContext: NSManagedObjectContext!
+        beforeEach {
+            managedObjectContext = createInMemoryManagedObjectContext()
+        }
+        
+        it("") {
+            
+        }
+    }
+    
+}
