24374d560500d222b6f967b4b7af12998ce80a81
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-15 21:02:27 +0300

    Реализован Intheam импортер, соответствующий протоколу importing

---
+++ TaskBerserk/IntheamImporter.swift
@@ -0,0 +1,45 @@
+//
+//  IntheamImporter.swift
+//  TaskBerserk
+//
+//  Created by Александр on 15.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import RxSwift
+
+class IntheamImporter: Importing {
+    var managedObjectContext: NSManagedObjectContext!
+    var webService: TaskWebService
+    
+    init(context: NSManagedObjectContext, webService: TaskWebService) {
+        self.managedObjectContext = context
+        self.webService = webService
+    }
+    
+    func importTasks() {
+        webService.fetchAllTask()
+            .subscribeNext { response in
+                for task in response.tasks {
+                    self.managedObjectContext.performChanges {
+                        Task.updateOrCreateTask(
+                            inContext: self.managedObjectContext,
+                            name: task.name,
+                            project: task.project,
+                            id: task.id,
+                            status: task.status,
+                            priority: task.priority,
+                            dueDate: task.dueDate,
+                            urgency: task.urgency,
+                            tags: task.tags)
+                    }
+                }
+                
+            }
+            .addDisposableTo(disposeBag)
+    }
+    
+    private var disposeBag = DisposeBag()
+}
