0b07e61f776b19f17b7e4c852fd331c92aa4ea27
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-16 11:13:07 +0300

    Добавлены пустые реализации taskChangeMetaViewModel-ing

---
+++ TaskBerserk/TaskChangeMetaViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskChangeMetaViewModel.swift
+//  TaskBerserk
+//
+//  Created by Александр on 16.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class TaskChangeMetaViewModel: TaskChangeMetaViewModeling {
+    
+}
