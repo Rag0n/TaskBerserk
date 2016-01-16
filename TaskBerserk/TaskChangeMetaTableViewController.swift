fa684a9688084810db0c963954070f3b09989736
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-16 11:49:06 +0300

    Добавлена пустая реализация

---
+++ TaskBerserk/TaskChangeMetaTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TaskChangeMetaTableViewController.swift
+//  TaskBerserk
+//
+//  Created by Александр on 16.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class TaskChangeMetaTableViewController: UITableViewController {
+    
+}
