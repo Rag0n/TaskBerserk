3b608eada01951e81574bbf3bc2134887cf7ccec
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-16 11:35:17 +0300

    Добавлены пустые реализации

---
+++ TaskBerserk/TaskChangeMetaViewCellModeling.swift
@@ -0,0 +1,13 @@
+//
+//  TaskChangeMetaViewCellModeling.swift
+//  TaskBerserk
+//
+//  Created by Александр on 16.01.16.
+//  Copyright © 2016 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+protocol TaskChangeMetaViewCellModeling {
+    
+}
