c3bc5cc25bd01095cabbd0da417fd241607cd349
Alexander Guschin <alexander.ragon@gmail.com>
2016-01-18 14:03:26 +0300

    Базовая реализация с DataSource

--- TaskBerserk/TaskChangeProjectViewController.swift
+++ TaskBerserk/TaskChangeProjectViewController.swift
@@ -11,5 +11,43 @@ import RxCocoa
 import RxSwift
 
 class TaskChangeProjectViewController: UITableViewController {
+    var viewModel: TaskChangeProjectViewModeling!
+}
 
+// MARK: UITableViewDataSource
+extension TaskChangeProjectViewController {
+    
+    func processUpdates(updates: [DataProviderUpdate<TaskChangeMetaViewCellModeling>]?) {
+        guard let updates = updates else { return }
+        tableView.beginUpdates()
+        for update in updates {
+            switch update {
+            case .Insert(let indexPath):
+                tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            case .Update(let indexPath, let cellViewModel):
+                guard let cell = tableView.cellForRowAtIndexPath(indexPath) as? TaskChangeMetaTableViewCell else { break }
+                cell.viewModel = cellViewModel
+            case .Move(let indexPath, let newIndexPath):
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Fade)
+            case .Delete(let indexPath):
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            }
+        }
+        tableView.endUpdates()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsInSection(section)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(
+            viewModel.cellIdentifier, forIndexPath: indexPath) as! TaskChangeMetaTableViewCell
+        
+        let cellViewModel = viewModel.viewModelForIndexPath(indexPath)
+        cell.viewModel = cellViewModel
+        
+        return cell
+    }
 }
